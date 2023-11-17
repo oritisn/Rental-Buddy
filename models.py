c303107c46a726d1db3604754d4180458840a915
oritisn <127325934+oritisn@users.noreply.github.com>
2023-11-17 17:15:17 +0000

    Changed models to new ones
    Created model for Users, subclasses for Landlords and Tenants, and established relationships between them

--- models.py
+++ models.py
@@ -1,40 +1,65 @@
 from datetime import datetime
+
+from sqlalchemy import ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db
 
-Category_User = db.Table('category_user',
-                         db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
-                         db.Column("category_id", db.Integer, db.ForeignKey("category.id")))
 
-Course_User = db.Table('course_user',
-                       db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
-                       db.Column("course_id", db.Integer, db.ForeignKey('course.id')))
-
-Class_User = db.Table("class_user",
-                      db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
-                      db.Column("class_id", db.Integer, db.ForeignKey('class.id')))
-
-
-class Course(db.Model):
+# Category_User = db.Table('category_user',
+#                          db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+#                          db.Column("category_id", db.Integer, db.ForeignKey("category.id")))
+#
+# Course_User = db.Table('course_user',
+#                        db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+#                        db.Column("course_id", db.Integer, db.ForeignKey('course.id')))
+#
+# Class_User = db.Table("class_user",
+#                       db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+#                       db.Column("class_id", db.Integer, db.ForeignKey('class.id')))
+#
+#
+# class Course(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(200), nullable=False)
+#
+#     def __repr__(self):
+#         return f'<Courses "{self.id}   {self.name}">'
+#
+#
+#
+#
+#
+# class Category(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(200), nullable=False)
+#
+#     def __repr__(self):
+#         return f'<Category "{self.name}">'
+#
+#
+class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f'<Courses "{self.id}   {self.name}">'
+Property_Tenant = db.Table("property_tenant",
+                           db.Column("property_id", db.Integer, db.ForeignKey('property.id')),
+                           db.Column("tenant_id", db.Integer, db.ForeignKey('tenant.tenant_id'))
+                           )
+
+Property_Landlord = db.Table("property_landlord",
+                             db.Column("property_id", db.Integer, db.ForeignKey('property.id')),
+                             db.Column("landlord_id", db.Integer, db.ForeignKey('landlord.landlord_id'))
+                             )
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
-    first_name = db.Column(db.String(200))
-    last_name = db.Column(db.String(200))
     email = db.Column(db.String(200), nullable=False, unique=True)
+    security_number = db.Column(db.String(200), nullable=False, unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow())
-
-    category = db.relationship("Category", secondary=Category_User, backref="User")
-    courses = db.relationship('Course', secondary=Course_User, backref="User")
 
     def set_password(self, password):
         """Create hashed password."""
@@ -49,33 +74,35 @@ class User(UserMixin, db.Model):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
-    def __init__(self, username, password, category, email):
+    def __init__(self, username, password, email, security_number):
         self.username = username
         self.password = self.set_password(password)
-        # self.first_name = first_name
-        # self.last_name = last_name
         self.email = email
-        self.category.append(db.session.query(Category).filter(Category.name == category).first())
+        self.security_number = security_number
 
     def __repr__(self):
         return f'User: {self.username}' \
                f'first name: {self.first_name}' \
                f'lastname: {self.last_name}' \
                f'email: {self.email}' \
-               f'dateadded: {self.date_added}'
+               f'date added: {self.date_added}' \
+               f'security number: {self.security_number}'
 
 
-class Category(db.Model):
+class Tenant(db.Model):
+    tenant_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.relationship("user", backref="Tenant", lazy=True)
+    # properties = db.Column()
+
+class Landlord(db.Model):
+    landlord_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.relationship("user", backref="Tenant", lazy=True)
+
+
+class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f'<Category "{self.name}">'
-
-
-class Class(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String, nullable=False)
+    # add columns for other details here
 
 
 def recreate_all_databases():
@@ -90,19 +117,6 @@ def recreate_all_databases():
     print("Dropped")
     db.create_all()
     print("Created")
-
-    for course in ('IELTS', 'B1', 'Grammar'):
-        ToAdd = Course(name=course)
-        db.session.add(ToAdd)
-        del ToAdd
-    print(Course.query.all())
-    print("Courses added")
-    for category in ('Teacher', 'Student'):
-        ToAdd = Category(name=category)
-        db.session.add(ToAdd)
-        del ToAdd
-    print(Category.query.all())
-    print("Categories added")
     db.session.commit()
     print("Committed")
 
